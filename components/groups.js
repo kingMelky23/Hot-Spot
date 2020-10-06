dbbd560b6489b3dd6361a9fa659e2a3881e45594
king <tettehmel@gmail.com>
2020-10-05 20:22:02 -0400

    added image

--- components/groups.js
+++ components/groups.js
@@ -11,6 +11,12 @@ import Group from "./group";
 
       <View>
         <Text style={styles.groupListingTitle}>Group Listings</Text>
+        <Image // images should be sent in as prop from single page event
+            style={{flex:1, height: undefined, width: undefined}}
+            source={require('../assets/shakeshack.png')}
+            resizeMode="contain"
+          />
+
         <FlatList
         style={styles.card}
         data={props.groupList}
