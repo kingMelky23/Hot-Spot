5b3c68a2dfe5af0fc3450cad01203f215323eab1
king <tettehmel@gmail.com>
2020-10-11 20:52:16 -0400

    changed navigation order

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,44 +1,42 @@
-import React from 'react'
+import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 
-import Event from '../screens/EventPage'
-import GroupPage from '../screens/GroupPage'
-import Header from '../shared/header'
-import Home from '../screens/Home'
+import Event from "../screens/EventPage";
+import GroupPage from "../screens/GroupPage";
+import Header from "../shared/header";
+import Home from "../screens/Home";
 
 const screens = {
-    EventPage: {
-        screen: Event,
-        navigationOptions: ({navigation})=>{
-            return{
-                headerTitle: ()=> <Header navigation={navigation} title='Event '/>
-            }
-        }
+  Home: {
+    screen: Home,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: () => <Header navigation={navigation} title="Hot Spot" />,
+      };
     },
-    Home: {
-        screen: Home,
-        navigationOptions: ({navigation})=>{
-            return{
-                headerTitle: ()=> <Header navigation={navigation} title='Hot Spot'/>
-            }
-        }
+  },
+  EventPage: {
+    screen: Event,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: () => <Header navigation={navigation} title="Event " />,
+      };
     },
-    GroupPage:{
-        screen: GroupPage
-    }
-}
+  },
+  GroupPage: {
+    screen: GroupPage,
+  },
+};
 
-const HomeStack = createStackNavigator(screens,{
-    defaultNavigationOptions:{
-        title: "Hot Spot",
-            headerStyle:{
-                backgroundColor: '#FF5555',
-                height:160,
-                borderWidth: 0
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    title: "Hot Spot",
+    headerStyle: {
+      backgroundColor: "#FF5555",
+      height: 160,
+      borderWidth: 0,
+    },
+  },
+});
 
-            },
-            
-    }
-})
-
-export default HomeStack
+export default HomeStack;
