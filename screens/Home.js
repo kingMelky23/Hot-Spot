7270450ad4158610107b23da715398d7791b1b29
Derrick C <derrick.chow234@gmail.com>
2020-10-28 21:02:58 -0400

    Connected route to homepage

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet, FlatList, Text, View , Image,TouchableOpacity} from 'react-native'
 import { globalStyles } from "../styles/globalStyles";
+import axios from "axios"
 
 var faker = require("faker");
 
@@ -27,6 +28,37 @@ export default function Home({navigation}) {
         key:"3"
     }]);
 
+    const [longitude, setLongitude] = useState();
+    const [latitude, setLatitude] = useState(); 
+
+    async function findEvents(lat, long) { 
+        await axios.get(`https://hotspot-backend.herokuapp.com/api/v1/get/FindEventsNearCoordinates?lat=${lat}&long=${long}`)
+        .then((res) => {
+            console.log("HI")
+            console.log("-------------------------------------------------------------")
+            console.log(res.data);
+            changeLocations(res.data.events);
+        }).catch((err) => {
+            console.log(err);
+        })
+        
+    }
+
+    useEffect(()=>{
+        const geoLocation = async() => {
+            await axios.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyD0uqCj-8Hr4IegcMZ4NVGzPSQmhmEAZk4")
+        .then((res)=>{
+            setLongitude(res.data.location.lng);
+            setLatitude(res.data.location.lat);
+            findEvents(latitude,longitude);
+        })
+        .catch((err)=>{console.log(err)})}
+        geoLocation();
+    },[])
+
+ 
+
+
     const pressHandler = () =>{
         navigation.navigate('EventPage',)
     }
@@ -40,11 +72,11 @@ export default function Home({navigation}) {
                 <TouchableOpacity onPress={()=>navigation.navigate('EventPage',item)}>
                     <View style={styles.boxView}>       
                         <Image style={styles.img} source={{
-                            uri:item.locationPhoto
+                            uri:item.photo_url
                         }}/>
                         <View>
-                            <Text style={styles.head}> {item.locationName} </Text>
-                            <Text style={styles.groups}> {item.numberOfGroups} Groups</Text>
+                            <Text style={styles.head}> {item.name} </Text>
+                            <Text style={styles.groups}> {item.groups.length} Groups</Text>
                         </View>
                     </View>
                 </TouchableOpacity>
