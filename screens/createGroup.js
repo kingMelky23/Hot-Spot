e05e1df249144fa63147be92cb81848c146b5553
king <tettehmel@gmail.com>
2020-11-15 15:16:07 -0500

    pulled from master

--- screens/createGroup.js
+++ screens/createGroup.js
@@ -79,81 +79,6 @@ export default function CreateGroup({ addGroup }) {
             </Text>
             {/* <Text >Date</Text> */}
 
-<<<<<<< HEAD
-export default function CreateGroup({addGroup}) {
-    return (
-      <View style={{...globalStyles.card,marginTop: 10}}>
-        <Formik
-          initialValues={{
-            name: "",
-            description: "",
-            participants: '4',
-            capacity: "",
-            location: "",
-            date: "",
-            time: "",
-            members:[{}]
-          }}
-          validationSchema={groupSchema}
-          onSubmit={(values, actions)=>{
-            actions.resetForm();
-            addGroup(values)
-            
-          }}
-        >
-            {(formikProps)=>(
-                <View>
-                    <Text>Name:</Text>
-                    <TextInput
-                        style={globalStyles.input}
-                        placeholder="Group Name"
-                        onChangeText={formikProps.handleChange('name')}
-                        value={formikProps.values.name}
-                        onBlur ={formikProps.handleBlur('name')}
-                    />
-                        <Text style={globalStyles.errorText}>{formikProps.touched.name && formikProps.errors.name}</Text>
-                        <Text>Description:</Text>
-                    <TextInput
-                        multiline
-                        style={globalStyles.input}
-                        placeholder="Description"
-                        onChangeText={formikProps.handleChange('description')}
-                        value={formikProps.values.description}
-                        onBlur ={formikProps.handleBlur('description')}
-                    />
-                        <Text style={globalStyles.errorText}>{formikProps.touched.description &&formikProps.errors.description}</Text>
-                        <Text>Capacity:</Text>
-                    <TextInput
-                        
-                        style={globalStyles.input}
-                        placeholder="2-16"
-                        onChangeText={formikProps.handleChange('capacity')}
-                        value={formikProps.values.capacity}
-                        keyboardType={"numeric"}
-                        onBlur ={formikProps.handleBlur('capacity')}
-                    />
-                        <Text style={globalStyles.errorText}>{formikProps.touched.capacity && formikProps.errors.capacity}</Text>
-                        <Text>Date</Text>
-                    <TextInput
-                        style={globalStyles.input}
-                        placeholder="01/02/2020"
-                        onChangeText={formikProps.handleChange('date')}
-                        value={formikProps.values.date}
-                    />
-                    <TextInput
-                        style={[globalStyles.input,{marginBottom: 50}]}
-                        placeholder="12:00am"
-                        onChangeText={formikProps.handleChange('time')}
-                        value={formikProps.values.time}
-                    />
-                    <FlatButton text="submit" onPress={formikProps.handleSubmit}/>
-                    {/* <Button title="submit" color="#FF5555" onPress={formikProps.handleSubmit}/> */}
-                </View>
-            )}
-        </Formik>
-      </View>
-    );
-=======
             <View style={styles.timeFormat}>
               <DateTimeFormat name="startDate" title="Start Date" />
               <DateTimeFormat name="endDate" title="End Date" />
@@ -165,7 +90,6 @@ export default function CreateGroup({addGroup}) {
       </Formik>
     </View>
   );
->>>>>>> 4cd78c1a5ae5d3518172518882d23a06fa828c5b
 }
 
 const styles = StyleSheet.create({
