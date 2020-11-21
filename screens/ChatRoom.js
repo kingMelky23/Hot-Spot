a0439b057dee312192dd768d55bf82544f786245
king <tettehmel@gmail.com>
2020-11-20 21:55:19 -0500

    started tab navigation

---
+++ screens/ChatRoom.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ChatRoom() {
+    return (
+        <View>
+            <Text>CHAT ROOM</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/GroupChat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function GroupChat() {
+    return (
+        <View>
+            <Text>GroupChat.js</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
