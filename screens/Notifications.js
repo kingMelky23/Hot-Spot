2fa6de02818e1d152c4ab0e6c38a2d36418a9d10
king <tettehmel@gmail.com>
2020-11-25 16:18:30 -0500

    added slide feature to notification

--- screens/Notifications.js
+++ screens/Notifications.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect} from 'react'
-import { StyleSheet, Text, View,Image,FlatList, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View,Image,FlatList, TouchableOpacity,Animated } from 'react-native'
 import {ListItem, Icon} from "react-native-elements"
+import { SwipeListView } from "react-native-swipe-list-view";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { FontAwesome } from '@expo/vector-icons'; 
 import { not } from 'react-native-reanimated'
 
@@ -10,10 +12,10 @@ var faker = require("faker");
 export default function Notifications() {
 
     const [notify,setNotifty] = useState([
-        {name: faker.name.findName(), request: "join", image:faker.image.people(), hours:faker.random.number({'min':1, 'max':23})},
-        {name: faker.name.findName(), request: "join", image:faker.image.people(), hours:faker.random.number({'min':1, 'max':23})},
-        {name: faker.name.findName(), request: "join", image:faker.image.people(), hours:faker.random.number({'min':1, 'max':23})},
-        {name: faker.name.findName(), request: "join", image:faker.image.people(), hours:faker.random.number({'min':1, 'max':23})},
+        {name: faker.name.findName(), request: "join", image:faker.image.people(), hours:faker.random.number({'min':1, 'max':23}), key:"1"},
+        {name: faker.name.findName(), request: "join", image:faker.image.people(), hours:faker.random.number({'min':1, 'max':23}), key:"2"},
+        {name: faker.name.findName(), request: "join", image:faker.image.people(), hours:faker.random.number({'min':1, 'max':23}), key:"3"},
+        {name: faker.name.findName(), request: "join", image:faker.image.people(), hours:faker.random.number({'min':1, 'max':23}), key:"4"},
     ])
 
     useEffect(() => {
@@ -25,6 +27,95 @@ export default function Notifications() {
         console.log(err)
       })
     },[])
+
+
+  
+    const readRow = (rowMap, rowKey) => {
+      if (rowMap[rowKey]) {
+        rowMap[rowKey].closeRow();
+      }
+    };
+  
+    const deleteRow = (rowMap, rowKey) => {
+      closeRow(rowMap, rowKey);
+      /**
+       * REACT NATIVE SWIPE TO DELETE TUTORIAL
+       * TIMESTAMP: 13:40
+       *
+       * USE DELETE FUNCTION FROM BACK END
+       */
+    };
+  
+    const HiddenItemWithActions = (props) => {
+      const { swipeAnimatedValue,onRead, onDelete} = props;
+  
+      return (
+        <View style={styles.rowBack}>
+          <TouchableOpacity
+            style={[styles.backLeftBtn, styles.backLeftBtnLeft]}
+            onPress={onRead}
+          >
+
+            <Text>Read</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.backRightBtn, styles.backRightBtnLeft]}
+            onPress={onRead}
+          >
+            <Animated.View style={[styles.rightButtonIcons,{
+              transform: [
+                {
+                  scale: swipeAnimatedValue.interpolate({
+                    inputRange: [-90, -45],
+                    outputRange: [1, 0],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }]}>
+              <MaterialCommunityIcons
+                name="close-circle-outline"
+                size={25}
+                color="#FFF"
+              />
+            </Animated.View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.backRightBtn, styles.backRightBtnRight]}
+            onPress={onDelete}
+          >
+            <Animated.View style={[styles.rightButtonIcons,{
+              transform: [
+                {
+                  scale: swipeAnimatedValue.interpolate({
+                    inputRange: [-90, -45],
+                    outputRange: [1, 0],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }]}>
+              <MaterialCommunityIcons
+                name="trash-can-outline"
+                size={25}
+                color="#FFF"
+              />
+            </Animated.View>
+          </TouchableOpacity>
+        </View>
+      );
+    };
+  
+    const renderHiddenItem = (data, rowMap) => {
+      return (
+        <HiddenItemWithActions
+          data={data}
+          rowMap={rowMap}
+          onRead={() => readRow(rowMap, data.item.key)}
+          onDelete={() => deleteRow(rowMap, data.item.key)}
+        />
+      );
+    };
 
 
 
@@ -42,9 +133,13 @@ export default function Notifications() {
     
       return (
         <View style={styles.container}>
-          <FlatList 
-            data ={notify}
-            renderItem = {renderItem}
+          <SwipeListView
+            data={notify}
+            renderItem={renderItem}
+            renderHiddenItem={renderHiddenItem}
+            leftOpenValue={90}
+            rightOpenValue={-150}
+            disableLeftSwipe
           />
         </View>
       )
@@ -55,7 +150,43 @@ export default function Notifications() {
       borderRadius:70,
       height:50,
       width:50
-    }
+    },
+    rowBack: {
+      alignItems: "center",
+      backgroundColor: "#DDD",
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingLeft: 15,
+      
+      marginRight: 20,
+      borderRadius: 5,
+    },
+    backLeftBtn: {
+      alignItems: "flex-start",
+      bottom: 0,
+      justifyContent: "center",
+      position: "absolute",
+      top: 0,
+      width: 90,
+      paddingRight: 17,
+    },
+    backLeftBtnLeft: {
+      alignItems:"center",
+      backgroundColor: "#1f65ff",
+      left: 0,
+    },
+    backRightBtnRight: {
+      backgroundColor: "red",
+      right: 0,
+      borderTopRightRadius: 5,
+      borderBottomRightRadius: 5,
+    },
+    rightButtonIcons: {
+      height: 25,
+      width: 25,
+      marginRight: 7,
+    },
   });
     
                                                 
